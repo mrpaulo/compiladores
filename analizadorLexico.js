0dbb3df5d039ec734e8b1a6d30043ff562c244a1
Lincon Reis <lincon9891@gmail.com>
2017-10-17 15:32:36 -0200

    programa pronto em console

--- analizadorLexico.js
+++ analizadorLexico.js
@@ -48,6 +48,62 @@ var _loadFile = function () {
 var _analyzeFile = function(){
     console.log("Reader: "+ fileLoaded);//esse fileLoad é o que tem o conteudo do arquivo nele que vamos trabalhar para botar a tabela
     //esse esquema daqui de baixo tirei do trabalho de IA que usamos pra mostrar o resultado do algoritmo do caxeiro viajante
+
+
+
+    // Expressão regular que reconhece as variaveis prog 1 e prog 2------------
+    var reVariavel = /\w+ = /g;
+    var str1 = fileLoaded;
+    var myArray1;
+
+    while ((myArray1 = reVariavel.exec(str1)) !== null) {
+        var result1 = 'Variáveis encontradas: ' + myArray1[0] + '. ';
+        result1 += 'Finalizada na posição: ' + reVariavel.lastIndex;
+        console.log(/\n/, result1);
+    }
+
+    //------------------------------------------------------------------------
+
+    // Palavras reservadas prog 1 e prog 2 -----------------------------------
+    var rePalavraResev = /enquanto|faca|escreve/g;
+    var str2 = fileLoaded;
+    var myArray2;
+
+    while ((myArray2 = rePalavraResev.exec(str2)) !== null) {
+        var result2 = 'Palavras reservadas encontradas: ' + myArray2[0] + '. ';
+        result2 += 'Na posição: ' + rePalavraResev.lastIndex;
+        console.log(result2);
+    }
+
+    //------------------------------------------------------------------------
+
+    // Operadores lógicos prog1 e prog 2 -------------------------------------
+    var reOpeLogic = /\-|\+|\*|\//g;
+    var str3 = fileLoaded;
+    var myArray3;
+
+    while ((myArray3 = reOpeLogic.exec(str3)) !== null) {
+        var result3 = 'Operadores lógicos encontrados: ' + myArray3[0] + '. ';
+        result3 += 'Na posição: ' + reOpeLogic.lastIndex;
+        console.log("----->" + result3);
+    }
+    //-------------------------------------------------------------------------
+
+    // Demilimitadores prog 1 e prog 2-----------------------------------------
+    var reDelimitadores = /\(\w*|w*\)|{w*|\w*}/g;
+    var str4 = fileLoaded;
+    var myArray4;
+
+    while ((myArray4 = reDelimitadores.exec(str4)) !== null) {
+        var result4 = 'Delimitadores encontrados: ' + myArray4[0] + '. ';
+        result4 += 'Na posição: ' + reDelimitadores.lastIndex;
+        console.log("<(((((()))))))>" + result4);
+    }
+    //------------------------------------------------------------------------
+
+    // Terminei aqui!! Em console mostra tudo e funciona perfeitamente eu acho heheh. Só por na tabela
+
+
     var tbody =document.querySelector("#result-table");
     tbody.innerHTML = "";
     var id = 10;//quantidade de lexemas  
